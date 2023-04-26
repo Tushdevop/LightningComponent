95c80be0aacdf0aca489f7775d078fea02bf770d
Tushdevop <120645045+Tushdevop@users.noreply.github.com>
2023-04-26 22:02:40 +0530

    final

--- force-app/main/default/aura/ErrorHandlingComp/ErrorHandlingCompController.js
+++ force-app/main/default/aura/ErrorHandlingComp/ErrorHandlingCompController.js
@@ -1,37 +1,36 @@
 ({
     throwError : function(component, event, helper) {
-          //  this.throwError('Error thrown'); 
-          var hasPerm=false;
+           var hasPerm=false;
 
           try {
             if (!hasPerm) {
                 throw new Error("You dont have permission");
             }
-          } catch (error) {
+          } catch (e) {
                 $A.createComponents([
-                        [
-                            "ui:message" , {
-                                "Title" : "Sample Error",
+                        ["ui:message" , {
+                                "title" : "Sample Error",
                                 "severity" : "error"
-                            }
-                        ],
-                        [
-                            "ui:outputText", {
-                                "value" : error.message
-                            }
-                        ],
-
-                        function(component, status, errorMessage){
+                            }],
+                        ["ui:outputText", {
+                                "value" : e.message
+                          }]
+						],
+                        function(components, status, errorMessage){
                             if (status === "SUCCESS") {
-                                var message=component[0];
-                                var outputText=component[1];
+                                var message=components[0];
+                                var outputText=components[1];
 
                                 message.set("v.body", outputText);
-                                var div1=component.find("div1");
+                                var div1 = component.find("div1");
                                 div1.set("v.body", message);
+                            }else if(status === "INCOMPLETE"){
+                                console.log("No response from server");
+                            }else if(status === "ERROR"){
+                                console.log("Error "+errorMessage);
                             }
                         }
-                    ])
+                    );
           }
     }
 })
